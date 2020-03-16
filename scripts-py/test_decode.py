0006c3d9d04be116cf39a160e1e81487185a737f
Keeps-on <1299793997@qq.com>
2020-03-16 16:28:23 +0800

    整理文件

---
+++ scripts-py/test_decode.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/15 10:18
+# @Author  : Lee
+# @Email  : 1299793997@qq.com
+# @File  : test_decode.py 
+# @参考  :
+
+
+"""
+编码：是把文本（字符串）转换成字节流，Unicode 格式转换成其他编码格式
+    encode英文原意是编码
+解码：是把字节流转换成字符串（文本），其他编码格式转成 Unicode
+    decode英文意思是解码
+
+python 2.x默认的字符编码是ASCII，默认的文件编码也是ASCII。
+python 3.x默认的字符编码是unicode，默认的文件编码是utf-8。
+"""
+
+
